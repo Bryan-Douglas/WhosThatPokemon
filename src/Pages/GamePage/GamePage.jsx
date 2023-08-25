351e555f831225c78a7cd67d1c14b3f86b54dca8
Bryan-Douglas <brydougwork@gmail.com>
2023-08-25 01:23:32 -0700

    added pokemonBio component, connected it to the api, styled it.

--- src/Pages/GamePage/GamePage.jsx
+++ src/Pages/GamePage/GamePage.jsx
@@ -2,6 +2,7 @@ import './GamePage.scss';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import PokemonBio from '../../Components/PokemonBio/PokemonBio';
 
 function GamePage() {
 
@@ -9,13 +10,29 @@ function GamePage() {
   const [activePokemon, setActivePokemon] = useState({
     id: null,
     spriteUrl: null,
-    name: null
+    name: null,
+    types: null,
+    height: null,
+    weight: null,
+    genera: null,
+    flavor_text_entries: null
   });
 
   const [inputValue, setInputValue] = useState('');
   const [feedback, setFeedback] = useState('');
   const [showCorrect, setShowCorrect] = useState(false);
   const [correctGuesses, setCorrectGuesses] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPokemon, setSelectedPokemon] = useState([]);
+
+  function openModal(pokemon) {
+    setSelectedPokemon(pokemon);
+    setShowModal(true);
+  }
+
+  function closeModal() {
+    setShowModal(false);
+  }
 
   useEffect(() => {
     const randomPokemonId = generateRandomPokemonId();
@@ -30,12 +47,23 @@ function GamePage() {
     try {
       const randomPokemonId = generateRandomPokemonId();
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`);
+      const altResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${randomPokemonId}`);
       const spriteUrl = response.data.sprites.other['official-artwork'].front_default;
       const name = response.data.name;
+      const types = response.data.types.map(typeObj => typeObj.type.name);
+      const height = response.data.height;
+      const weight = response.data.weight;
+      const genera = altResponse.data.genera[7].genus;
+      const flavourText = altResponse.data.flavor_text_entries[0].flavor_text;
       setActivePokemon({
         id: randomPokemonId,
         spriteUrl: spriteUrl,
-        name: name
+        name: name,
+        types: types,
+        height: height,
+        weight: weight,
+        genera: genera,
+        flavor_text_entries: flavourText
       });
     } catch (error) {
       console.error('Error retrieving active pokemon data', error);
@@ -119,12 +147,13 @@ function GamePage() {
         </div>
         <div className='pokedex-container'>
           {correctGuesses.map((pokemon, index) => (
-            <div className='pokedex-card' key={index}>
+            <div className='pokedex-card' key={index} onClick={() => openModal(pokemon)}>
               <img className='pokedex-avatar' src={pokemon.spriteUrl} alt={pokemon.name} />
             </div>
           ))}
         </div>
       </div>
+      {showModal && (<PokemonBio closeModal={closeModal} pokemon={selectedPokemon} />)}
     </>
   )
 }
