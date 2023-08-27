483fa947fc00c09c820e64c62be1e708b5607911
Bryan-Douglas <brydougwork@gmail.com>
2023-08-27 04:44:21 -0700

    added functions to convert height and weight into imperial measurements. made the bulbapedia link clickable.

--- src/Components/PokemonBio/PokemonBio.jsx
+++ src/Components/PokemonBio/PokemonBio.jsx
@@ -1,16 +1,30 @@
+import { useEffect } from 'react';
 import './PokemonBio.scss';
 
 function PokemonBio({ closeModal, pokemon }) {
 
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if (event.key === 'Escape') {
+                closeModal();
+            }
+        };
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+        window.removeEventListener('keydown', handleKeyDown);   
+    }
+    }, [closeModal]);
+
     function capitalizeAndJoinTypes(types) {
         if (!types || types.length === 0) {
-          return '';
+            return '';
         }
         return types.map(type => capitalizeFirstLetter(type)).join(' / ');
-      }
-      function capitalizeFirstLetter(string) {
+    }
+    function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
-      }
+    }
 
     return (
         <>
@@ -33,13 +47,13 @@ function PokemonBio({ closeModal, pokemon }) {
                         <div className='pokemon-rightWrapper'>
                             <div className='pokemon-stats__wrapper'>
                                 <div className='pokemon-types__box'>
-                                <p className='pokemon-types'>{capitalizeAndJoinTypes(pokemon.types)}</p>
+                                    <p className='pokemon-types'>{capitalizeAndJoinTypes(pokemon.types)}</p>
                                 </div>
                                 <div className='pokemon-height__box'>
-                                    <p className='pokemon-height'>HT. {pokemon.height}</p>
+                                    <p className='pokemon-height'>HT. {pokemon.height.feet}' {pokemon.height.inches}"</p>
                                 </div>
                                 <div className='pokemon-weight__box'>
-                                    <p className='pokemon-weight'>WT. {pokemon.weight}</p>
+                                    <p className='pokemon-weight'>WT. {pokemon.weight.pounds} lbs</p>
                                 </div>
                                 <button className='modal-closeButton' onClick={closeModal}>X</button>
                             </div>
@@ -50,7 +64,7 @@ function PokemonBio({ closeModal, pokemon }) {
                                 <p className='pokemon-flavourText'>{pokemon.flavor_text_entries}</p>
                             </div>
                             <div className='pokemon-link__box'>
-                                <p className='pokemon-link'>https://bulbapedia.bulbagarden.net/wiki/{pokemon.name}</p>
+                            <a className='pokemon-link' href={`https://bulbapedia.bulbagarden.net/wiki/${pokemon.name}`}>https://bulbapedia.bulbagarden.net/wiki/{pokemon.name}</a>
                             </div>
                         </div>
                     </div>
@@ -61,8 +75,3 @@ function PokemonBio({ closeModal, pokemon }) {
 }
 
 export default PokemonBio;
-
-/* 
-https://pokeapi.co/api/v2/pokemon-species/{id or name}
-endpoint for the descriptons
-*/
